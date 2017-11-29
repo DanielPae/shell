83b84964ba4ecfcea68f9cf1b1aa37f6fb5c553c
unknown <danielpae104@gmail.com>
2017-11-29 15:00:36 -0500

    runs commands, loops until user exits, TONS of comments, more modular functions, commands can be separated by ;, forks of children to run commands

--- shell.c
+++ shell.c
@@ -7,34 +7,95 @@
 #include "shell.h"
 
 
-char* getNextImput(){
-	char* ans = malloc(1000);	
+char* get_next_imput(){
+	static char ans[1000];	
+	printf("COMMAND:");
 	fgets(ans, 1000, stdin);
 	return ans;
 }
 
-char ** parse_args ( char * line ){
+int ready_string ( char *str ){
+	int len = strcspn(str, "\n");
+	str[len] = 0;
+	return len + 1;
+}
+
+char ** split_args ( char *line ){
+	char *split = " ";
+	return parse( line, split);
+} 
+
+char ** split_commands ( char *line ){
+	char *split = ";";
+	return parse( line, split);
+} 
+
+char ** split_redirect ( char *line ){
+	char *split = ">";
+	return parse( line, split);
+} 
+
+char ** parse ( char *line, char *split ){
   char ** args = (char **) malloc(sizeof(char *) * 6);
   int i = 0;
   while(line){
-    args[i] = strsep(&line, " ");
+    args[i] = strsep(&line, split);
     i++;
   }
   args[i] = NULL;
   return args;
 }
 
+int run_command( char **args){
+	if(!strcmp("exit", args[0])){
+		//ENDS THE PROGRAM
+		printf("Was it something I said?\n");
+		exit(0);
+	}	
+	else if(!strcmp("cd", args[0])){
+		//WILL RUN CD STUFFS HERE
+	}
+	else{
+		//RUNS THE COMMAND
+		int child = fork();
+		if(child){
+			wait();
+		}
+		else{
+			return execvp(args[0], args);
+		}
+	}
+}
+
 int main(){
-	char *run = getNextImput();
-	char mutable[50];
-	int len = strcspn(run, "\n");
-	strncpy(mutable, run, len);
-	mutable[len] = 0;
-	char **args = parse_args(mutable);
-	execvp(args[0], args);
-	printf("%s\n", mutable);
-	printf("%d\n", len);
-	printf("%s\n", strerror(errno));
+
+	// VARIABLES:
+	// imput -> stores user imput as a string
+	// args -> stores char ** to put into execvp
+
+	
+	//RUN UNTIL USER EXITS
+	while(1){
+		//get user imput and put it into imput
+		char *imput = get_next_imput();
+
+		//gets the string ready to strsep
+		ready_string(imput);
+
+		//splits the string into seprate commands
+		char **commands = split_commands(imput);
+	
+		//loop through all the commands and run them
+		int i = 0;
+		while(commands[i]){
+			//split commands into its seprate arguments
+			char **args = split_args(commands[i]);
+			//run the command
+			run_command(args);
+			//next command
+			i++;
+		}
+	}
 }
 
 
--- shell.h
+++ shell.h
@@ -1,3 +1,20 @@
-char* getNextImput();
-char** parse_args(char *);
+// uses fgets to get the next user imput
+char* get_next_imput();
 
+// removes \n chars from the end of a string
+int ready_string(char *);
+
+// splits a string on its white spaces and returns a char ** of the new strings
+char** split_args(char*);
+
+// splits a string on ; and returns a char ** of the new strings
+char** split_commands(char*);
+
+// splits a string on > and returns a char ** of the new strings
+char** split_redirect(char*);
+
+// helper function for all of the splits. Splits first argument on second argument 
+char** parse(char *, char *);
+
+// forks of a child process and runs execvp. Returns 1 if there is a problem
+int run_command(char**);
