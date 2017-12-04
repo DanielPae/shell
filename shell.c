72ccf4e1331bcc4dda17ed99531537229e02c66b
unknown <danielpae104@gmail.com>
2017-12-04 00:32:14 -0500

    done

--- shell.c
+++ shell.c
@@ -4,20 +4,32 @@
 #include <time.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "shell.h"
 
 
 char* get_next_imput(){
 	static char ans[1000];	
-	printf("COMMAND:");
+	char path[258];
+	getcwd(path, 255);
+	printf("%s: ", path);
 	fgets(ans, 1000, stdin);
 	return ans;
 }
 
-int ready_string ( char *str ){
-	int len = strcspn(str, "\n");
+char * ready_string ( char *str ){
+	int blanks = 0;
+	while(str[blanks] == ' '){
+		blanks++;
+	}
+	str = str + blanks;
+	int len = strcspn(str, "\n\0");
+	while(str[len - 1] == ' ' || str[len - 1] == '0' && len > -1){
+		len--;
+	}
 	str[len] = 0;
-	return len + 1;
+	return str;
 }
 
 char ** split_args ( char *line ){
@@ -27,16 +39,50 @@ char ** split_args ( char *line ){
 
 char ** split_commands ( char *line ){
 	char *split = ";";
-	return parse( line, split);
+	char **commands = parse( line, split);
+	int i = 0;
+	while(commands[i]){
+		commands[i] = ready_string(commands[i]);
+		i++;
+	}
+	return commands;
 } 
 
-char ** split_redirect ( char *line ){
+char ** split_redirect_out ( char *line ){
 	char *split = ">";
-	return parse( line, split);
+	char **commands = parse( line, split);
+	int i = 0;
+	while(commands[i]){
+		commands[i] = ready_string(commands[i]);
+		i++;
+	}
+	return commands;
+} 
+
+char ** split_redirect_in ( char *line ){
+	char *split = "<";
+	char **commands = parse( line, split);
+	int i = 0;
+	while(commands[i]){
+		commands[i] = ready_string(commands[i]);
+		i++;
+	}
+	return commands;
+} 
+
+char ** split_pipe ( char *line ){
+	char *split = "|";
+	char **commands = parse( line, split);
+	int i = 0;
+	while(commands[i]){
+		commands[i] = ready_string(commands[i]);
+		i++;
+	}
+	return commands;
 } 
 
 char ** parse ( char *line, char *split ){
-  char ** args = (char **) malloc(sizeof(char *) * 6);
+  char ** args = (char **) malloc(sizeof(char *) * 10);
   int i = 0;
   while(line){
     args[i] = strsep(&line, split);
@@ -46,6 +92,52 @@ char ** parse ( char *line, char *split ){
   return args;
 }
 
+int my_cd(char * filename){
+	char path[258];
+	getcwd(path, 255);
+	strcat(path, "/");
+	strcat(path, filename);
+	chdir(path);
+}
+
+void re_out(char ** command){
+	char path[258];
+	getcwd(path, 255);
+	strcat(path, "/");
+	strcat(path, command[1]);
+	char **args = split_args(command[0]);
+	redirect(args, path, STDOUT_FILENO);
+}
+
+void re_in(char ** command){
+	char path[258];
+	getcwd(path, 255);
+	strcat(path, "/");
+	strcat(path, command[1]);
+	char **args = split_args(command[0]);
+	redirect(args, path, STDIN_FILENO); 
+}
+
+void redirect(char ** function, char * destination, int in_out){
+	int holder = dup(in_out);
+	int asd = open(destination, O_RDWR | O_CREAT, 0644);
+	int dest = dup2(asd, in_out);
+	run_command(function);
+	dup2(holder, dest);
+}
+
+void pipe_it( char ** pipeable){
+	int holder = dup(STDIN_FILENO);
+	FILE* f = popen(pipeable[0], "r");
+	int asd = fileno(f);
+	int dest = dup2(asd, STDIN_FILENO);
+	char **args = split_args(pipeable[1]);
+	run_command(args);
+	dup2(holder, dest);
+	fclose(f);
+	
+}
+
 int run_command( char **args){
 	if(!strcmp("exit", args[0])){
 		//ENDS THE PROGRAM
@@ -53,7 +145,8 @@ int run_command( char **args){
 		exit(0);
 	}	
 	else if(!strcmp("cd", args[0])){
-		//WILL RUN CD STUFFS HERE
+		//CDS THE STUFFS
+		my_cd(args[1]);
 	}
 	else{
 		//RUNS THE COMMAND
@@ -62,7 +155,8 @@ int run_command( char **args){
 			wait();
 		}
 		else{
-			return execvp(args[0], args);
+			execvp(args[0], args);
+			exit(0);
 		}
 	}
 }
@@ -80,20 +174,37 @@ int main(){
 		char *imput = get_next_imput();
 
 		//gets the string ready to strsep
-		ready_string(imput);
+		imput = ready_string(imput);
 
 		//splits the string into seprate commands
-		char **commands = split_commands(imput);
-	
+		char **commands = split_commands(imput);	
+
 		//loop through all the commands and run them
 		int i = 0;
 		while(commands[i]){
 			//split commands into its seprate arguments
+			if(strchr(commands[i], '>')){
+				char **split = split_redirect_out(commands[i]);
+				re_out(split);
+				i++;
+			}
+			else if(strchr(commands[i], '<')){
+				char **split = split_redirect_in(commands[i]);
+				re_in(split);
+				i++;
+			}
+			else if(strchr(commands[i], '|')){
+				char **split = split_pipe(commands[i]);
+				pipe_it(split);
+				i++;
+			}
+			else{
 			char **args = split_args(commands[i]);
 			//run the command
 			run_command(args);
 			//next command
 			i++;
+			}
 		}
 	}
 }
--- shell.h
+++ shell.h
@@ -1,20 +1,47 @@
-// uses fgets to get the next user imput
 char* get_next_imput();
+	// No imputs
+	// Returns a string of the users imput 
 
-// removes \n chars from the end of a string
-int ready_string(char *);
+char* ready_string(char *);
+	// Takes a mutable string
+	// Removes \n chars from the end of a string and blank space from front and end and returns it
 
-// splits a string on its white spaces and returns a char ** of the new strings
 char** split_args(char*);
+	// Takes a mutable string
+	// Splits a string on its white spaces and returns a char ** of the new strings
 
-// splits a string on ; and returns a char ** of the new strings
 char** split_commands(char*);
+	// Takes a mutable string
+	// Splits a string on ; and returns a char ** of the new strings
 
-// splits a string on > and returns a char ** of the new strings
-char** split_redirect(char*);
+char** split_redirect_out(char*);
+	// Takes a mutable string
+	// Splits a string on > and returns a char ** of the new strings
 
-// helper function for all of the splits. Splits first argument on second argument 
+char** split_redirect_in(char*);
+	// Takes a mutable string
+	// Splits a string on < and returns a char ** of the new strings
+
 char** parse(char *, char *);
+	// Helper function for all of the splits. Splits first argument on second argument 
 
-// forks of a child process and runs execvp. Returns 1 if there is a problem
+void my_cd(char * filename);
+	// Takes a string
+	// Changes directory 
+
+void re_out(char **);
+	//redirects the output of the fuction contained in the char ** to stdout
+
+void re_in(char **);
+	//redirects the output of the fuction contained in the char ** to stdin
+
+void redirect(char **, char *, int);
+	// Takes a char *[2], a file name as a string, and the fd of the place to redirect it to 
+	// Redirects the output of a given function into the second argument. Helper function for re_out and re_in
+
+void pipe_it(char **);
+	// Takes a char *[2] with char[0] and char[1] being commands. Redirects the output of char[0] into the imput of char[1]
+
 int run_command(char**);
+	// Forks of a child process and runs execvp. Returns 1 if there is a problem
+
